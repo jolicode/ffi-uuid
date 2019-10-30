1b410c1cea77d7b5283d8c3b357e95f84a09af17
Baptiste Leduc <baptiste.leduc@gmail.com>
2019-10-30 21:44:25 +0100

    Add GH action with bench

---
+++ bench.php
@@ -0,0 +1,18 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+
+$count = 1 * 1000 * 1000;
+$factory = new Korbeil\Uuid\UuidFactory();
+
+$s = microtime(true);
+for ($i = 0; $i < $count; ++$i) {
+    $factory->v1();
+}
+printf("Bench [v1] %.3fs - ", microtime(true) - $s);
+
+$s = microtime(true);
+for ($i = 0; $i < $count; ++$i) {
+    $factory->v4();
+}
+printf("[v4] %.3fs", microtime(true) - $s);
