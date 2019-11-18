dccca46ea7592f21f0c562f159a3518480e88cf0
Grégoire Pineau <lyrixx@lyrixx.info>
2019-11-18 12:20:44 +0100

    CS

--- include/uuid-php.php
+++ include/uuid-php.php
@@ -1,16 +1,12 @@
 <?php
 
-declare(strict_types=1);
-
-namespace {
-    /**
-     * @method FFI\CData new(string $type)
-     * @method void uuid_generate_random(FFI\CData $out)
-     * @method void uuid_generate_time(FFI\CData $out)
-     * @method void uuid_generate_md5(FFI\CData $out, FFI\CData $ns, string $name, int $len)
-     * @method void uuid_generate_sha1(FFI\CData $out, FFI\CData $ns, string $name, int $len)
-     */
-    final class JoliCodeUuidPhp/* extends FFI */
-    {
-    }
+/**
+ * @method FFI\CData new(string $type)
+ * @method void uuid_generate_random(FFI\CData $out)
+ * @method void uuid_generate_time(FFI\CData $out)
+ * @method void uuid_generate_md5(FFI\CData $out, FFI\CData $ns, string $name, int $len)
+ * @method void uuid_generate_sha1(FFI\CData $out, FFI\CData $ns, string $name, int $len)
+ */
+final class JoliCodeUuidPhp extends FFI
+{
 }
