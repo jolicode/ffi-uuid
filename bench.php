08d2d7228c4852f4b91157b2e91187390d6049ba
Grégoire Pineau <lyrixx@lyrixx.info>
2019-11-18 15:32:59 +0100

    Update bench to use PECL too

--- bench.php
+++ bench.php
@@ -5,16 +5,32 @@ require __DIR__.'/vendor/autoload.php';
 $count = 1 * 1000 * 1000;
 $generator = new JoliCode\Uuid\UuidGenerator();
 
+print "FFI:\n";
+
 $s = microtime(true);
 for ($i = 0; $i < $count; ++$i) {
     $generator->v1();
 }
-printf("Bench [v1] %.3fs - ", microtime(true) - $s);
+printf(" * [v1] %.3fs\n", microtime(true) - $s);
 
 $s = microtime(true);
 for ($i = 0; $i < $count; ++$i) {
     $generator->v4();
 }
-printf("[v4] %.3fs", microtime(true) - $s);
+printf(" * [v4] %.3fs\n", microtime(true) - $s);
 
-print("\n");
+if (function_exists('uuid_create')) {
+    print "PECL:\n";
+
+    $s = microtime(true);
+    for ($i = 0; $i < $count; ++$i) {
+        uuid_create(UUID_TYPE_TIME);
+    }
+    printf(" * [v1] %.3fs\n", microtime(true) - $s);
+
+    $s = microtime(true);
+    for ($i = 0; $i < $count; ++$i) {
+        uuid_create(UUID_TYPE_RANDOM);
+    }
+    printf(" * [v4] %.3fs\n", microtime(true) - $s);
+}
